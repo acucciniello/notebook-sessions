f5f46623c991d3ffe96770342f40cf0c3218794b
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2016-12-05 13:44:19 -0500

    Add a button meant to be in textarea
    Add a button to "Save Thoughts" for when the user is done typing.  Not
    complete.  It is outside of the textarea, I need it inside.

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,22 @@ class App extends Component {
   render () {
     return <div>
       <TextBox placeholder='Write your thoughts here to clear your mind...' />
+      <BoxButton type='button' value='Save Your Thoughts' name='Save Your Thoughts' />
     </div>
   }
 }
 
 export default App
+
+var BoxButton = React.createClass({
+  render: function () {
+    return <div className={'box-button'}>
+      <button type={this.props.type} value={this.props.value} name={this.props.name}>
+      Save Your Thoughts
+      </button>
+    </div>
+  }
+})
 
 var TextBox = React.createClass({
   getInitialState: function () {
@@ -20,7 +31,7 @@ var TextBox = React.createClass({
 
   render: function () {
     return <div className={'text-box'}>
-        <textarea type={this.props.type || 'text'} placeholder={this.props.placeholder} />
+      <textarea type={this.props.type || 'text'} placeholder={this.props.placeholder} />
     </div>
   }
 })
