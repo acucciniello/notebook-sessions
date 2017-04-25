ca04814a6affe92876a6e645b498c0f01a0eef14
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-04-25 14:09:47 -0400

    Add Text to top and bottom of page, need to format

--- app/home-page/home-page.js
+++ app/home-page/home-page.js
@@ -9,14 +9,26 @@ function RenderHome (h, state) {
   var homePageStyle = style[state.viewport] || style.base
   // var LoginEmitter = state.emitters.login
   // create two text areas and a button in a div
+  var top = h('p', {
+  }, 'notebook-sessions')
+  var topDiv = h('div', {
+    className: 'topdiv',
+    id: 'myTopDiv'
+  }, [top])
   var nbImage = h('img', {
     className: 'nb',
     src: 'https://s3.amazonaws.com/notebook-sessions/notebook_sessions.jpeg',
     alt: 'notebook picture'
   }, 'notebook-picture')
+  var bottom = h('p', {
+  }, 'Write Down Your Problems with a Worry in the World')
+  var bottomDiv = h('div', {
+    className: 'bottomdiv',
+    id: 'myBottomDiv'
+  }, [bottom])
   var renderedHome = h('div', {
     className: 'homepage',
     style: homePageStyle
-  }, [nbImage])
+  }, [topDiv, nbImage, bottomDiv])
   return renderedHome
 }
