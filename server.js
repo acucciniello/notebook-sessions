f3bca8e7cf6496a19c68f985fc716f0f5be380d4
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-04-27 19:08:59 -0400

    Add Salting and Hashing to Password storage for Security
        TODO: Refactor and Modularize Backend Code

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var jwt = require('jsonwebtoken')
 var getTimeStamp = require('./get-timestamp.js')
 var timestamp = getTimeStamp()
 var jsonParser = bodyParser.json()
+var bcrypt = require('bcrypt')
+var saltRounds = 10
 // var textParser = bodyParser.text()
 var thought
 var config = {
@@ -44,22 +46,31 @@ app.post('/login', jsonParser, function (req, res) {
   var password = req.body.password
   console.log(req)
   console.log('We received this from the client: ' + email + ' ' + password)
-  var checkEmailInfo = format('SELECT * from accounts WHERE email = %L AND password = %L', email, password)
-  myClient.query(checkEmailInfo, function (err, result) {
+  var getHashedPass = format('SELECT * from accounts WHERE email = %L', email)
+  myClient.query(getHashedPass, function (err, result) {
     if (err) {
       console.log(err)
-    } else if (result.rowCount === 1) {
-      console.log(result)
-      var uid = result.rows[0].userid
-      var token = jwt.sign(req.body, app.get('superSecret'))
-      return res.json({
-        success: true,
-        message: 'Enjoy your token',
-        userid: uid,
-        token: token
+    } else {
+      var hash = result.rows[0].password
+      console.log(hash)
+      bcrypt.compare(password, hash, function (err, response) {
+        if (err) {
+          console.log(err)
+        } else if (response === true) {
+          var uid = result.rows[0].userid
+          var token = jwt.sign(req.body, app.get('superSecret'))
+          console.log(token)
+          return res.json({
+            success: true,
+            message: 'Enjoy your token',
+            userid: uid,
+            token: token
+          })
+        } else {
+          return res.end('done')
+        }
       })
     }
-    return res.end('done')
   })
 })
 
@@ -67,42 +78,47 @@ app.post('/signup', jsonParser, function (req, res) {
   var email = req.body.email
   var password = req.body.password
   console.log('We received this from the client: ' + email + ' ' + password)
-  var checkEmailInfo = format('SELECT * from accounts WHERE email = %L AND password = %L', email, password)
-  myClient.query(checkEmailInfo, function (err, result) {
+  bcrypt.hash(password, saltRounds, function (err, hash) {
     if (err) {
       console.log(err)
-      res.end('done')
-    } else if (result.rows[0] === undefined) {
-      console.log('the user does not exist need to add user')
-      var addUser = format('INSERT INTO accounts VALUES(%L, %L);', email, password)
-      myClient.query(addUser, function (err, result) {
-        if (err) {
-          console.log(err)
-          res.end('done')
-        } else {
-          var pullUID = format('SELECT * from accounts WHERE email = %L AND password = %L', email, password)
-          myClient.query(pullUID, function (err, result) {
-            if (err) {
-              res.end('done')
-            } else {
-              var uid = result.rows[0].userid
-              var token = jwt.sign(req.body, app.get('superSecret'))
-              return res.json({
-                success: true,
-                message: 'Enjoy your token',
-                userid: uid,
-                token: token
-              })
-            }
-          })
-        }
-      })
-    } else {
-      console.log('There is an account that exists with the email provided')
-      return res.json({ error: 'There is an account that already exists with the email provided' })
     }
+    var checkEmailInfo = format('SELECT * from accounts WHERE email = %L AND password = %L', email, hash)
+    myClient.query(checkEmailInfo, function (err, result) {
+      if (err) {
+        console.log(err)
+        res.end('done')
+      } else if (result.rows[0] === undefined) {
+        console.log('the user does not exist need to add user')
+        var addUser = format('INSERT INTO accounts VALUES(%L, %L);', email, hash)
+        myClient.query(addUser, function (err, result) {
+          if (err) {
+            console.log(err)
+            res.end('done')
+          } else {
+            var pullUID = format('SELECT * from accounts WHERE email = %L AND password = %L', email, hash)
+            myClient.query(pullUID, function (err, result) {
+              if (err) {
+                res.end('done')
+              } else {
+                var uid = result.rows[0].userid
+                var token = jwt.sign(req.body, app.get('superSecret'))
+                return res.json({
+                  success: true,
+                  message: 'Enjoy your token',
+                  userid: uid,
+                  token: token
+                })
+              }
+            })
+          }
+        })
+      } else {
+        console.log('There is an account that exists with the email provided')
+        return res.json({ error: 'There is an account that already exists with the email provided' })
+      }
+    })
+    return
   })
-  return
 })
 
 app.post('/thoughts', jsonParser, function (req, res, next) {
